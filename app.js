a5ece64311a89ba749de9660df1efdac5d156a0d
johnvmt <johnvmt@gmail.com>
2019-01-16 16:43:00 -0500

    Updated release rules

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.setLoginItemSettings({
 var robot = require("robotjs");
 
 // Speed up the mouse.
-robot.setMouseDelay(2);
+robot.setMouseDelay(1);
 
 var twoPI = Math.PI * 2.0;
 var screenSize = robot.getScreenSize();
